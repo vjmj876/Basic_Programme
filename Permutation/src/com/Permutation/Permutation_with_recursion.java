42b88e2831eef815a4cba97aa39ea71677a9be8b
volleyDon <volleyDon@ADMINRG-FV7I8EF>
2020-01-21 14:23:05 +0530

    completed

--- Permutation/src/com/Permutation/Permutation_with_recursion.java
+++ Permutation/src/com/Permutation/Permutation_with_recursion.java
@@ -1,43 +1,33 @@
 package com.Permutation;
 
 public class Permutation_with_recursion {
+	
+	private static void swap(char[] ch, int i, int j)
+	{
+		char temp = ch[i];
+		ch[i] = ch[j];
+		ch[j] = temp;
+	} 
+	
+	private static void permutations(char[] ch, int currentIndex)
+	{
+		if (currentIndex == ch.length - 1) {
+			System.out.println(String.valueOf(ch));
+		}
 
-	 static int count=1 ;
+		for (int i = currentIndex; i < ch.length; i++)
+		{
+			swap(ch, currentIndex, i);
+			permutations(ch, currentIndex + 1);
+			swap(ch, currentIndex, i);
+		}
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String str="ABC";
-		char s[]=str.toCharArray();
-		int l = 0;
-		int r = s.length;
-		Permutation_with_recursion pwr =new Permutation_with_recursion();
-		pwr.permutation( s , l , r-1);
-		
+		String s = "ABC";
+		permutations(s.toCharArray(), 0);
 	}
-	public  void permutation(char[] s, int l , int r) {
-			
-			if(l==r) {
-				int Sr_no=count++;
-				System.out.print(Sr_no+": ");
-				System.out.println( s);
-			}
-			else {
-				for(int i=l; i<=r; i++) {
-					
- 				swap(s ,(s[0+l]),(s[0+i]));
- 				
-			permutation(s,l+1,r);
-			swap(s ,(s[0+l]),(s[0+i]));
-				}
-			}
-		}
-	public  void swap(char s[], char x , char y ) {
-			
-			char temp;
-			temp = x;
-			x=y;
-			y=temp;
-			
-		}
 
 }
